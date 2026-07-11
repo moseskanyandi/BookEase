1a7f2904c1c7b006f95873f85371d251079f30e4
Moses Kanyandi <moseskanyandi11@gmail.com>
2026-07-11 09:25:57 +0200

    Update theme colors to BookEase branding

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,14 +1,19 @@
 /**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ * BookEase brand colors.
+ * Primary: #0F2D6B (deep blue)
+ * Secondary: #F4B400 (gold/amber)
  */
-
 import { Platform } from 'react-native';
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const primary = '#0F2D6B';
+const secondary = '#F4B400';
+
+const tintColorLight = primary;
+const tintColorDark = secondary;
 
 export const Colors = {
+  primary,
+  secondary,
   light: {
     text: '#11181C',
     background: '#fff',
